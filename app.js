a8df1828a88c270ebbf54c921d99eed8fe49e17f
Jérôme Guiraud <jerome31guiraud@gmail.com>
2018-02-15 10:04:06 +0100

    verif si imput non nul

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 $(document).ready(function () {
-
+   $("#rejouer").hide();
    var nombreAleatoire = Math.floor(Math.random() * 11);
-   console.log(nombreAleatoire);
+   console.log("nombre Aléatoire", nombreAleatoire);
+   var essais = 0;
 
    $("#valider").on("click", function () {
+      essais += 1; // essais++ ou essais = essais + 1
+      console.log("nombre d'essais", essais);
       var nombreSaisi = $('#number').val();
-
-      if (nombreAleatoire == nombreSaisi) {
-         console.log("victoire");
-      }
-      if (nombreAleatoire < nombreSaisi) {
-         console.log("c'est plus petit");
-      }
-      if (nombreAleatoire > nombreSaisi) {
-         console.log("c'est plus grand");
+      if (nombreSaisi.length !== 0) {
+         if (nombreAleatoire == nombreSaisi) {
+            console.log("victoire");
+         }
+         if (nombreAleatoire < nombreSaisi && essais < 4) {
+            console.log("c'est plus petit");
+         }
+         if (nombreAleatoire > nombreSaisi && essais < 4) {
+            console.log("c'est plus grand");
+         }
+         if (essais >= 3) {
+            $("#valider").hide();
+            $("#rejouer").show();
+         }
+      } else {
+         console.log("saisir un nombre valide")
       }
 
    });
+
+   $("#rejouer").on("click", function () {
+      nombreAleatoire = Math.floor(Math.random() * 11);
+      console.log("nouveau nombre aléatoire", nombreAleatoire);
+      essais = 0;
+      console.log("nouveaux essais", essais);
+      $("#rejouer").hide();
+      $("#valider").show();
+      $("#number").val("");
+   })
 
 });
